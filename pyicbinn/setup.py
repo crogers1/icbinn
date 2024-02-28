23bad0240938655d881c2a8823c2b0d56a4d9740
Chris Rogers <crogers122@gmail.com>
2024-02-28 12:44:24 -0500

    [pyicbinn] kirkstone: replace distutils with setuptools
    Signed-off-by: Chris Rogers <crogers122@gmail.com>

--- pyicbinn/setup.py
+++ pyicbinn/setup.py
@@ -16,8 +16,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools import Extension
 #from subprocess import check_output
 import subprocess;
 
